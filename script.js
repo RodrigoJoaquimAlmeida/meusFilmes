a94fd6a5040bf23e4582fcc8b55a14db03b21ede
Rodrigo Joaquim Almeida <rjgalmeida@gmail.com>
2024-09-01 11:09:07 -0300

    Using Notie Package NPM for error

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
 const searchButton = document.getElementById('search-button');
 const overlay = document.getElementById('modal-overlay');
+const movieName = document.getElementById('movie-name');
+const movieYear = document.getElementById('movie-year');
 
-function searchButtonClickHandler() {
-	overlay.classList.add('open');
+//
+async function searchButtonClickHandler() {
+	try {
+		let url = `http://www.omdbapi.com/?apikey=${key}&t=${movieNameParameterGenerator()}${movieYearParameterGenerator()}`;
+		const response = await fetch(url);
+		const data = await response.json();
+		console.log('data: ', data);
+		if (data.Error) {
+			throw new Error('Filme não encontrado');
+		}
+
+		overlay.classList.add('open');
+	} catch (error) {
+		notie.alert({ type: 'error', text: error.message });
+	}
+}
+
+function movieNameParameterGenerator() {
+	if (movieName.value === '') {
+		throw new Error('Informe nome do Filme');
+	}
+	return movieName.value.split(' ').join('+');
+}
+
+function movieYearParameterGenerator() {
+	if (movieYear.value === '') {
+		return '';
+	} else if (
+		movieYear.value.length !== 4 ||
+		Number.isNaN(Number(movieYear.value))
+	) {
+		throw new Error('Ano inválido');
+	} else {
+		return `&y=${movieYear.value}`;
+	}
 }
 
 searchButton.addEventListener('click', searchButtonClickHandler);
